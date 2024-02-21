c6064126856b8bffa5cdd4e482890f1eed94e4d4
Pablo Monteagudo Lago <pmonteagudolago@gmail.com>
2024-02-21 15:57:04 +0000

    Improved experimentation pipeline

--- experiments/create_environment.sh
+++ experiments/create_environment.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-conda create -n clrs_env python=3.9
+conda create -y -n clrs_env python=3.11
 conda activate clrs_env
-pip install -r requirements/requirements.txt
-python setup.py install
-pip install jinja2
+conda install -y nvidia/label/cuda-12.3.0::cuda-toolkit
+conda install -y nvidia/label/cuda-12.3.0::libcusparse
+conda install -y -c nvidia cuda-nvcc
+pip install -y -r requirements/requirements.txt
+pip install -y jinja2
+pip uninstall -y jax jaxlib
+pip install -y -U "jax[cuda12_pip]==0.4.21" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+module avail nvidia-cuda-toolkit
+export LD_LIBRARY_PATH=~/.conda/envs/clrs_env/lib:$LD_LIBRARY_PATH
+export PATH=~/.conda/envs/clrs_env/bin:$PATH
--- experiments/experiment_generator.py
+++ experiments/experiment_generator.py
@@ -1,6 +1,8 @@
 import json
-from subprocess import call
+from subprocess import call, run
+import os
 from jinja2 import Template
+from pathlib import Path
 
 ###################################################
 # Only need to change this line experiments_to run
@@ -10,6 +12,10 @@ experiments_to_run = [0]
 # No need to change anything below here
 ###################################################
 
+# Retrieve CRSid
+CRSid = os.environ.get('USER')
+# Get the name of the currently active conda environment
+conda_environment = os.environ.get('CONDA_DEFAULT_ENV')
 # Experiments folder
 experiments_folder = "./experiments"
 # Checkpoints folder
@@ -30,7 +36,9 @@ with open(experiment_file_name, "r") as experiment_file:
         # Skip processing experiments which are not intended to be run
         if int(experiment_id) not in experiments_to_run:
             continue
-        experiment_options = f"--checkpoint_path {checkpoints_folder}/{experiment_id} "
+        experiment_options = (f"--checkpoint_path {checkpoints_folder}/{experiment_id} "
+                              f"--dataset_path /tmp/CLRS30/{experiment_id} "
+                              )
         for key, value in experiment_args.items():
             if type(value) == bool:
                 if value:
@@ -52,11 +60,23 @@ with open(experiment_file_name, "r") as experiment_file:
         template = Template(slurm_template_content)
 
         # Render the template with the variable value
-        rendered_template = template.render(experiment_options=experiment_options, experiment_id=experiment_id)
+        rendered_template = template.render(
+            experiment_options=experiment_options, 
+            experiment_id=experiment_id,
+            CRSid=CRSid,
+            conda_environment=conda_environment,
+        )
 
         # Write the rendered template to a new file or use it as needed
         with open(f"{experiments_folder}/{slurm_output_file_name}_{experiment_id}", "w") as slurm_output_file:
             slurm_output_file.write(rendered_template)
+
+# Full path to the new "logs" folder
+logs_path = Path(os.getcwd()) / 'logs'
+print(logs_path)
+# Check if the new directory exists, and if not, create it
+if not logs_path.exists():
+    logs_path.mkdir()
 
 # Iterate over the experiments running them
 for experiment_id in experiments_to_run:
