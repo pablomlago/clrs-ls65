bf20f5538a20fc768bfa5bfbac98ccd98a5268dd
Pablo Monteagudo Lago <pmonteagudolago@gmail.com>
2024-03-13 15:26:15 +0000

    Final experiments

--- experiments/experiment_generator.py
+++ experiments/experiment_generator.py
@@ -7,7 +7,7 @@ from pathlib import Path
 ###################################################
 # Only need to change this line experiments_to run
 ###################################################
-experiments_to_run = [197]
+experiments_to_run = [197, 198, 199]
 test_flag = False
 ###################################################
 # No need to change anything below here
